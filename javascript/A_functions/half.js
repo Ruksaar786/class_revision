438464a9a3be11b84596929865339628375fb706
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-02-22 13:52:02 +0200

    js revision

---
+++ javascript/A_functions/half.js
@@ -0,0 +1,12 @@
+// Write a function `half` that accepts a number as an argument. The function should return half of the
+// number.
+
+const half = function (n) {
+  return n / 2;
+};
+
+console.log(half(8)); // 4
+console.log(half(15)); // 7.5
+console.log(half(90)); // 45
+
+// number divided by 2
